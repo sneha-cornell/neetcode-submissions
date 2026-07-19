3027cecdd75e1aa2d9b60360c421f6a4489a61a8
sneha-cornell <149324313+sneha-cornell@users.noreply.github.com>
2026-07-18 17:03:06 -0700

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #find largest sum of elements in array (must be contiguous)
+        #elements can be negative ! - sliding window will not work 
+        #return sum 
+
+        curr_sum, max_sum = nums[0],nums[0]
+
+        for num in nums[1:]: 
+            curr_sum=max(num,curr_sum+num)
+            max_sum = max(curr_sum,max_sum)
+        return max_sum
